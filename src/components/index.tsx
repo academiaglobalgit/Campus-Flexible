b791e3f83e9f50fb45ac03dd65dd7454bd1b8f18
Jose Ornelas <joseornelaz@gmail.com>
2025-06-14 20:51:45 -0700

    fix nombre

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -4,7 +4,7 @@ export { default as MiRuta } from './pages/MiRuta/MiRuta';
 export { default as NotFound } from './pages/NotFound/NotFound';
 export { default as ToDo } from './pages/ToDo/ToDo';
 export { default as PreguntasFrecuentes } from './pages/PreguntasFrecuentes/PreguntasFrecuentes';
-export { default as AyudaLogin } from './pages/Ayuda/AyudaLogin';
+export { default as AyudaLogin } from './pages/ayuda/AyudaLogin';
 export { default as MainTemplate } from './templates/MainTemplate/MainTemplate';
 export { default as LoginPage } from './pages/Login/Login';
 export { default as ServiciosEscolares } from './pages/ServiciosEscolares/ServiciosEscolares';
