a6c83af09291522238f619038aa63560e4c9da09
Jose Ornelas <joseornelaz@gmail.com>
2025-06-21 00:52:48 -0700

    Pantalla de Ayuda Exterior Desktop

--- src/theme.tsx
+++ src/theme.tsx
@@ -193,8 +193,8 @@ const theme = createTheme({
   breakpoints: {
     values: {
       xs: 0,
-      sm: 640,
-      md: 1024,
+      sm: 600,
+      md: 900,
       lg: 1200,
       xl: 1536,
     },
