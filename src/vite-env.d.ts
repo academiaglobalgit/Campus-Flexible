9f1b9ec6563a0959b03b17ce23764001ba13d421
Jose Ornelas <joseornelaz@gmail.com>
2025-06-26 12:55:57 -0700

    Mi Perfil - Se cambiaron los icons de Material UI por los de Design System

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -4,3 +4,21 @@ declare module '*.svg?react' {
   const content: ReactComponent;
   export default content;
 }
+
+import '@mui/material/styles';
+
+declare module '@mui/material/styles' {
+  interface Palette {
+    white: Palette['primary'];
+  }
+
+  interface PaletteOptions {
+    white?: PaletteOptions['primary'];
+  }
+}
+
+declare module '@mui/material/SvgIcon' {
+  interface SvgIconPropsColorOverrides {
+    white: true;
+  }
+}
