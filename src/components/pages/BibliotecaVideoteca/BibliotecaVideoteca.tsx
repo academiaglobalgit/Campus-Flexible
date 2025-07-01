7c2a50db1b5e90e5991737ac2ce105ae8d542431
Jose Ornelas <joseornelaz@gmail.com>
2025-06-30 17:51:11 -0700

    Container Desktop, Refactorizacion

--- src/components/pages/BibliotecaVideoteca/BibliotecaVideoteca.tsx
+++ src/components/pages/BibliotecaVideoteca/BibliotecaVideoteca.tsx
@@ -1,18 +1,17 @@
 import React from "react";
-import { Box, Grid, Tab, Tabs, useMediaQuery, useTheme } from "@mui/material";
+import { Box, Tab, Tabs, useMediaQuery, useTheme } from "@mui/material";
 import { Videoteca } from "./Videoteca";
 import imgVideoteca from "../../../assets/videoteca.jpg";
 import imgBiblioteca from "../../../assets/biblioteca.jpg";
 import { Biblioteca } from "./Biblioteca";
 import TabPanel from "../../molecules/TabPanel/TabPanel";
-import { TituloIcon } from "../../molecules/TituloIcon/TituloIcon";
-import { TitleScreen } from "@constants";
-import { Typography } from "../../atoms/Typography/Typography";
 
-const VideotecaBiblioteca: React.FC = () => {
+import { DescripcionesPantallas, TitleScreen } from "@constants";
+import { ContainerDesktop } from "../../organisms/ContainerDesktop/ContainerDesktop";
+
+const BibliotecaVideoteca: React.FC = () => {
     const theme = useTheme();
     const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
-    const betweenDevice = useMediaQuery(theme.breakpoints.between('sm', 'md'));
     const [value, setValue] = React.useState(0);
 
     const handleChange = (_event: React.SyntheticEvent, newValue: number) => {
@@ -50,25 +49,18 @@ const VideotecaBiblioteca: React.FC = () => {
     );
 
     return(
-        <Box sx={[{ width: '100%', }, isMobile && { paddingTop:'28px', paddingBottom: '28px' }]}>
-          {
-            isMobile 
-            ? <Contents />
-            :
-              <Box sx={{width: { md: '60.2vw', display: 'flex', flexDirection: 'column', gap: '32px' }}}>
-                <Grid container sx={{ alignItems:'center'}}>
-                      <Grid size={{md: !betweenDevice ? 8 : 12}}>
-                          <TituloIcon Titulo={TitleScreen.BIBLIOTECA} fontSize="h2" />
-                          <Typography component="span" variant="body1">
-                              Bienvenido a nuestra Biblioteca y Videoteca Digital, un espacio cuidadosamente organizado donde encontrarás una amplia colección de recursos escritos y audiovisuales
-                          </Typography>
-                      </Grid>
-                </Grid>
+        isMobile 
+        ? 
+            <Box sx={[{ width: '100%', }, isMobile && { paddingTop:'28px', paddingBottom: '28px' }]}>
                 <Contents />
-              </Box>
-          }      
-        </Box>
+            </Box>
+        :
+            <ContainerDesktop title={TitleScreen.BIBLIOTECA} description={DescripcionesPantallas.BIBLIOTECA} containerSize="60.2vw" >
+                <Box sx={{pt:'12px'}}>
+                    <Contents />
+                </Box>
+            </ContainerDesktop>
     );
 };
 
-export default VideotecaBiblioteca;
+export default BibliotecaVideoteca;
