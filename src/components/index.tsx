f362ddbbe9edff030432f632c1e1dcdea26689da
ChampzHub <champz94tuxo@gmail.com>
2025-07-01 18:29:31 -0600

    agrego vista de TyC

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -20,5 +20,6 @@ export { default as Ayuda } from './pages/ayuda/Ayuda';
 export { default as PreguntasFrecuentesInternas } from './pages/PreguntasFrecuentes/PreguntasFrecuentesInternas';
 export { default as SalaConversacion } from './pages/SalaConversacion/SalaConversacion';
 export { default as Consejeria } from './pages/Consejeria/Consejeria';
+export { default as terminosCondiciones } from './pages/terminosCondiciones/terminosCondiciones';
 
 export * as Organisms from './organisms';
