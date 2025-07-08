e2649dc7495f435e12175fbde37638492fe8beed
Jose Ornelas <joseornelaz@gmail.com>
2025-07-08 12:48:11 -0700

    Manual Biblioteca

--- src/components/pages/BibliotecaVideoteca/Biblioteca.tsx
+++ src/components/pages/BibliotecaVideoteca/Biblioteca.tsx
@@ -3,12 +3,22 @@ import { TituloIcon } from "../../molecules/TituloIcon/TituloIcon";
 import { Document } from "../../../assets/icons";
 import { Box, Typography } from "@mui/material";
 import Button from "../../atoms/Button/Button";
+import { useGetManuales } from "../../../services/ManualesService";
 
 type BibliotecaProps = {
     data: BibliotecaVideoteca;
 }
 
 export const Biblioteca: React.FC<BibliotecaProps> = ({data}) => {
+    const {data: manual} = useGetManuales('Manual Biblioteca', 'alumnos');
+
+    const handleManualBiblioteca = () => {
+        if (manual) {
+            const manualUrl = manual.url;
+            window.open(manualUrl, '_blank');
+        }
+    };
+
     return(
         <>
           <TituloIcon Titulo={TitleScreen.BIBLIOTECA_VIRTUAL} Icon={ Document } />
@@ -22,7 +32,7 @@ export const Biblioteca: React.FC<BibliotecaProps> = ({data}) => {
                     <Button onClick={() => {}} fullWidth>Acceder</Button>
                 </>
                 <>
-                    <Button onClick={() => {}} fullWidth variant="outlined">Manual de Biblioteca</Button>
+                    <Button onClick={handleManualBiblioteca} fullWidth variant="outlined">Manual de Biblioteca</Button>
                 </>
             </Box>
           </Box>
