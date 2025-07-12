95912124e4a70b1eb5139cf9b2185b62d00ec7be
ChampzHub <champz94tuxo@gmail.com>
2025-07-12 12:29:24 -0600

    diseño desktop

--- src/components/pages/CursosActivosDetalles/CursosActivosDetalles.tsx
+++ src/components/pages/CursosActivosDetalles/CursosActivosDetalles.tsx
@@ -396,23 +396,26 @@ const CursosActivosDetalles: React.FC = () => {
     const ListaPendientes = (lista: any) => (
 
         <>
-            <Box sx={{ display: 'flex', flexDirection: 'column', mt: 5, alignItems: 'start', justifyContent: 'center', gap: '18px' }}>
+            <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'start', justifyContent: 'center', gap: '18px' }}>
                 <TituloIcon key={5} Titulo={'Lista de pendientes'} Icon={ListaTareas} />
             </Box>
 
-            <Box sx={isMobile ? {} : { display: 'flex', flexDirection: 'row', mt: 5, alignItems: 'start', justifyContent: 'space-around' }}>
+            <Box sx={isMobile ? { display: 'flex', flexDirection: 'column' } : { display: 'flex', flexDirection: 'row', alignItems: 'start', justifyContent: 'space-around', overflowX: 'scroll', marginTop: '20px', width: '100%', gap: '40px' }}>
 
                 {lista.map((listado: { lista: string; total: number; hechas: number }) => (
                     <>
 
-                        <Box sx={isMobile ? {} : { display: 'flex', flexDirection: 'column', mt: 5, alignItems: 'start', justifyContent: 'center' }}>
-                            <Box sx={{ display: 'flex', flexDirection: 'column', mt: 5, alignItems: 'start', justifyContent: 'center' }}>
+                        <Box sx={isMobile ? {} : { display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '20px' }}>
+
+
+                            <Box sx={{ width: '100%' }}>
                                 <Divider textAlign="center">
                                     <Typography component="span" variant="body2" color="primary">{listado.lista}</Typography>
                                 </Divider>
                             </Box>
 
-                            <Box sx={{ display: 'flex', flexDirection: 'column', mt: 5, alignItems: 'start', justifyContent: 'center' }}>
+
+                            <Box sx={isMobile ? { display: 'flex', flexDirection: 'column', alignItems: 'start', justifyContent: 'space-around' } : { display: 'flex', flexDirection: 'column', alignItems: 'start', justifyContent: 'center', width: '250px' }}>
                                 {[...Array(4)].map((_, index) => {
                                     let auxTitulo = '';
                                     let auxChec = false;
@@ -436,25 +439,30 @@ const CursosActivosDetalles: React.FC = () => {
                                     }
 
                                     return (
-
                                         <CheckBoxLabel
                                             key={index}
                                             text={`${auxTitulo} ${index + 1}`}
                                             place="start"
-                                            disabled={auxChec}
-                                            defaultChecked={auxDisabled}
+                                            defaultChecked={auxChec}
                                             sxProps={{
                                                 display: 'flex',
                                                 justifyContent: 'space-between',
                                                 width: '100%',
                                                 color: theme.palette.primary.main,
+                                                gap: '15px',
+                                                marginRight: '20px'
                                             }}
                                         />
                                     );
                                 })}
 
+
                             </Box>
                         </Box>
+                        <Box sx={isMobile ? {} : {
+                            height: '215px', border: 'solid 1px', color: theme.palette.primary.main, marginTop: '30px'
+                        }}></Box >
+
                     </>
                 ))}
             </Box>
