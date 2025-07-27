bfec7cdc5b08375753df8130fae2f1d0885e80ee
Jose Ornelas <joseornelaz@gmail.com>
2025-07-27 14:05:45 -0700

    Notificaciones

--- src/components/molecules/IconsTopBar/IconsTopBar.tsx
+++ src/components/molecules/IconsTopBar/IconsTopBar.tsx
@@ -25,7 +25,7 @@ export const IconsTopBar: React.FC = () => {
     }
 
     const handleAllNotifications = () => {
-        alert("Notificaciones");
+        navigate(AppRoutingPaths.NOTIFICACIONES);
         handleClose();
     }
 
