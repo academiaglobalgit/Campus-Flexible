08daadf4925ed1cf11a5dc481e8e775948be0fbb
Jose Ornelas <joseornelaz@gmail.com>
2025-07-30 14:19:02 -0700

    Terminos y Condiciones - Refactorizacion

--- src/components/pages/terminosCondiciones/terminosCondiciones.tsx
+++ src/components/pages/terminosCondiciones/terminosCondiciones.tsx
@@ -1,38 +1,32 @@
 import { TitleScreen } from "@constants";
 import { Typography } from "../../atoms/Typography/Typography";
 import { CheckBoxLabel } from "../../atoms/Checkbox/Checkbox";
-import { Box, Button } from "@mui/material";
-import theme from "../../../theme";
+import { Box, Button, useTheme } from "@mui/material";
 import { ContainerDesktop } from "../../organisms/ContainerDesktop/ContainerDesktop";
 
+
 const TerminosCondiciones: React.FC = () => {
+    const theme = useTheme();    
+
+    const textos = () =>(
+        <>
+            <p>Bienvenido(a) al campus, te deseamos mucho éxito en tus estudios.</p>
+            <p>Antes de iniciar las actividades de tus cursos, deberás seguir dos sencillos pasos:</p>            
+            <p>1. Leer y aceptar de conformidad los Lineamientos Internos y Normas de Control Escolar, el cual puedes leer haciendo clic aquí.</p>            
+            <p>2. Comprometerse a asumir los costos por los conceptos de estudio que se generen, realizando los depósitos en las cuentas bancarias autorizadas. Consulta los precios aquí.</p>            
+            <p>Para conocer los tipos de becas y formas de pago que te proporciona tu empresa te sugerimos acercarte a tu RH.</p>            
+            <p>3. En caso de haberte inscrito al programa con documentación electrónica, leer y aceptar de conformidad los Términos y Condiciones de entrega de documentación física original, las cuales puedes leer haciendo clic aquí.</p>            
+            <p>4. Para garantizar que cumplimos con las nuevas normas de privacidad y datos, nos gustaría asegurarnos de que estés conforme de recibir actualizaciones por correo electrónico de Academia Global. Nuestro Aviso de Privacidad, mismo que puede ser encontrado aquí.</p>
+        </>
+    );
 
     const contenido = (
         <Box sx={{ display: 'flex', flexDirection: 'column', gap: '20px' }}>
             <Typography component="span" variant="h4">
                 Estimado(a) alumno(a):
             </Typography>
-            <Typography component="p" variant="body2">
-                Bienvenido(a) al campus, te deseamos mucho éxito en tus estudios.
-            </Typography>
-            <Typography component="p" variant="body2">
-                Antes de iniciar las actividades de tus cursos, deberás seguir dos sencillos pasos:
-            </Typography>
-            <Typography component="p" variant="body2">
-                1. Leer y aceptar de conformidad los Lineamientos Internos y Normas de Control Escolar, el cual puedes leer haciendo clic aquí.
-            </Typography>
-            <Typography component="p" variant="body2">
-                2. Comprometerse a asumir los costos por los conceptos de estudio que se generen, realizando los depósitos en las cuentas bancarias autorizadas. Consulta los precios aquí.
-            </Typography>
-
-            <Typography component="p" variant="body2">
-                Para conocer los tipos de becas y formas de pago que te proporciona tu empresa te sugerimos acercarte a tu RH.
-            </Typography>
-            <Typography component="p" variant="body2">
-                3. En caso de haberte inscrito al programa con documentación electrónica, leer y aceptar de conformidad los Términos y Condiciones de entrega de documentación física original, las cuales puedes leer haciendo clic aquí.
-            </Typography>
-            <Typography component="p" variant="body2">
-                4. Para garantizar que cumplimos con las nuevas normas de privacidad y datos, nos gustaría asegurarnos de que estés conforme de recibir actualizaciones por correo electrónico de Academia Global. Nuestro Aviso de Privacidad, mismo que puede ser encontrado aquí.
+            <Typography component="div" variant="body2">
+                { textos() }
             </Typography>
 
             <Typography component="p" variant="body3" sxProps={{ color: theme.palette.primary.main }}>
