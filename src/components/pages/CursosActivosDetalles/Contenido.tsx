f01ee9f3b3a30c4651245f60f3075a0380888cb9
ChampzHub <champz94tuxo@gmail.com>
2025-07-31 10:46:01 -0600

    cambio acordeon por cutomized

--- src/components/pages/CursosActivosDetalles/Contenido.tsx
+++ src/components/pages/CursosActivosDetalles/Contenido.tsx
@@ -5,34 +5,38 @@ import { Accordion } from "../../molecules/Accordion/Accordion";
 import { Box } from "@mui/material";
 import { toRoman } from "../../../utils/Helpers";
 import { accordionStyle } from "@styles";
+import { AccordionStatus } from "../../molecules/AccordionStatus/AccordionStatus";
 
 export const Contenido: React.FC = () => {
-    const {id} = useParams<{id:string}>();    
-    const {data: contenido, isLoading} = useGetCursosTabs(Number(id!), "Contenido");
+    const { id } = useParams<{ id: string }>();
+    const { data: contenido, isLoading } = useGetCursosTabs(Number(id!), "Contenido");
 
-    return(
+    return (
         isLoading ?
             <LoadingCircular Text="Cargando Contenido..." />
-        :
+            :
 
-         Object.entries(contenido).map(([unidad, contenidos], index) =>
-            <Accordion key={index} title={`Unidad ${toRoman(Number(unidad))}`} sxProps={accordionStyle}>
-                {
-                    contenidos.filter((item) => item.unidad === Number(unidad)).map((item, i) => (
-                        <Box key={i}>
-                            <iframe
-                                src={item?.url}
-                                style={{
-                                    width: "100%",
-                                    height: "100vh",
-                                    border: "none",
-                                }}
-                            />
-                        </Box>
-                    ))
-                }
-                
-            </Accordion>      
-        )
+            Object.entries(contenido).map(([unidad, contenidos], index) =>
+                <Accordion key={index}
+                    customHeader={<AccordionStatus tittle={`Unidad ${toRoman(Number(unidad))}`} status={'Cursando'} />}
+
+                    sxProps={accordionStyle}>
+                    {
+                        contenidos.filter((item) => item.unidad === Number(unidad)).map((item, i) => (
+                            <Box key={i}>
+                                <iframe
+                                    src={item?.url}
+                                    style={{
+                                        width: "100%",
+                                        height: "100vh",
+                                        border: "none",
+                                    }}
+                                />
+                            </Box>
+                        ))
+                    }
+
+                </Accordion>
+            )
     )
 };
