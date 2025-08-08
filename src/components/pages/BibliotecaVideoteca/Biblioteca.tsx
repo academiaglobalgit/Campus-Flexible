2b2a40172751c69f13296c983f78d084a1e4982b
ChampzHub <champz94tuxo@gmail.com>
2025-08-08 17:10:33 -0600

    cambios titulo y quito comentario

--- src/components/pages/BibliotecaVideoteca/Biblioteca.tsx
+++ src/components/pages/BibliotecaVideoteca/Biblioteca.tsx
@@ -1,9 +1,7 @@
 import { TitleScreen, type BibliotecaVideoteca } from "@constants";
 import { TituloIcon } from "../../molecules/TituloIcon/TituloIcon";
-import { Document } from "../../../assets/icons";
 import { Box } from "@mui/material";
 import Button from "../../atoms/Button/Button";
-//import { useGetManuales } from "../../../services/ManualesService";
 import { innerHTMLStyle } from "@styles";
 
 type BibliotecaProps = {
@@ -11,14 +9,6 @@ type BibliotecaProps = {
 }
 
 export const Biblioteca: React.FC<BibliotecaProps> = ({ data }) => {
-    //const { data: manual } = useGetManuales('Manual Biblioteca', 'alumnos');
-
-    /* const handleManualBiblioteca = () => {
-        if (manual) {
-            const manualUrl = manual.url;
-            window.open(manualUrl, '_blank');
-        }
-    }; */
 
     const handleOpenRedalyc = () => {
         window.open("https://www.redalyc.org", '_blank');
@@ -26,16 +16,13 @@ export const Biblioteca: React.FC<BibliotecaProps> = ({ data }) => {
 
     return (
         <>
-            <TituloIcon Titulo={TitleScreen.BIBLIOTECA_VIRTUAL} Icon={Document} />
+            <TituloIcon Titulo={TitleScreen.BIBLIOTECA_VIRTUAL} fontSize="h2" />
             <Box>
                 <Box sx={{ ...innerHTMLStyle }} dangerouslySetInnerHTML={{ __html: data.descripcion_html }} />
                 <Box sx={{ paddingTop: '10px', display: 'flex', gap: '15px', justifyContent: 'space-between' }}>
                     <>
                         <Button onClick={handleOpenRedalyc} fullWidth>Acceder</Button>
                     </>
-                    {/* <>
-                    <Button onClick={handleManualBiblioteca} fullWidth variant="outlined">Manual de Biblioteca</Button>
-                </> */}
                 </Box>
             </Box>
         </>
