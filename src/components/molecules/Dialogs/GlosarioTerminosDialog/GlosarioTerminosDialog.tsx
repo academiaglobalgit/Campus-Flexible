669c56b10cca6cb05974b349cd6ddd5a6c94f931
Jose Ornelas <joseornelaz@gmail.com>
2025-08-08 17:04:17 -0700

    Glosario

--- src/components/molecules/Dialogs/GlosarioTerminosDialog/GlosarioTerminosDialog.tsx
+++ src/components/molecules/Dialogs/GlosarioTerminosDialog/GlosarioTerminosDialog.tsx
@@ -3,13 +3,15 @@ import React, { useEffect } from "react";
 import Button from "../../../atoms/Button/Button";
 import { Dialog } from "../../../atoms/Dialog/Dialog";
 import { Typography } from "../../../atoms/Typography/Typography";
+import type { Glosario } from "../../../../types/Calificaciones.interface";
 
 type GlosarioDialogProps = {
     isOpen?: boolean;
+    glosario?: Glosario[];
     close: () => void;
 }
 
-export const GlosarioTerminosDialog: React.FC<GlosarioDialogProps> = ({isOpen, close}) => {
+export const GlosarioTerminosDialog: React.FC<GlosarioDialogProps> = ({isOpen, glosario, close}) => {
     const theme = useTheme();
     const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
     const [open, setOpen] = React.useState(false);
@@ -49,18 +51,11 @@ export const GlosarioTerminosDialog: React.FC<GlosarioDialogProps> = ({isOpen, c
                     <Typography component="span" variant="body2" color="primary">Glosario de Términos</Typography>
                 </Divider>
                 <Box sx={{display: 'flex', flexDirection: 'column', gap: '25px', paddingBottom: '16px'}}>
-                    <Typography component="span" variant="body2" color="primary">
-                        Periodo: <Typography component="span" variant="body1">Es la forma en la que vienen agrupadas las materias por bloques de 4, 5 o más materias.</Typography>
-                    </Typography>
-                    <Typography component="span" variant="body2" color="primary">
-                        Examen de módulo: <Typography component="span" variant="body1">Evaluación online que presentarás en tu plataforma cuando concluyas un módulo de tu curso, está compuesto por 10 a 20 preguntas.</Typography>
-                    </Typography>
-                    <Typography component="span" variant="body2" color="primary">
-                        Calificación Parcial: <Typography component="span" variant="body1">Es el promedio que obtendrás de todos los exámenes de módulo que hayas presentado durante cada asignatura, el cual equivale al 40% de tu Calificación Final, el cual verás reflejado en un máximo de 4 puntos.</Typography>
-                    </Typography>
-                    <Typography component="span" variant="body2" color="primary">
-                        Examen Final: <Typography component="span" variant="body1">Es una evaluación online que deberás presentar cuando concluyas los contenidos de todo curso o materia. Está compuesto por 20 o 25 preguntas y equivale al 60% de tu Calificación Final y la verás representada con un máximo de 6 puntos.</Typography>
-                    </Typography>
+                    {glosario && glosario.map((item) => (
+                        <Typography key={item.id_glosario} component="span" variant="body2" color="primary">
+                            {item.termino}: <Typography component="span" variant="body1">{item.descripcion}</Typography>
+                        </Typography>
+                    ))}
                 </Box>
                 {closeButton}
                 <Box sx={{paddingTop:'10px'}}></Box>
