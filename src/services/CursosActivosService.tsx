69ee35b9fae37bc1f2c4b5ee1fd9900fbe09b86e
Jose Ornelas <joseornelaz@gmail.com>
2025-08-27 10:46:29 -0700

    Eliminacion de linea hardcode

--- src/services/CursosActivosService.tsx
+++ src/services/CursosActivosService.tsx
@@ -35,11 +35,6 @@ export const useGetCursosTabs = (id: number, tab: string) => {
             if (!acc[contenido.unidad]) {
                 acc[contenido.unidad] = [];
             }
-
-            if(contenido.id_modulo === 1) {
-                contenido.estatus = "Finalizado";
-            }
-
             acc[contenido.unidad].push(contenido);
             return acc;
             }, {});
