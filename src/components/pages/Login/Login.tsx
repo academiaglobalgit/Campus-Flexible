4ef884acb6bdc0eccc9f9d644ce3e95bc4689999
ChampzHub <champz94tuxo@gmail.com>
2025-09-18 17:10:10 -0600

    add img diplomados

--- src/components/pages/Login/Login.tsx
+++ src/components/pages/Login/Login.tsx
@@ -13,7 +13,7 @@ import { useNavigate } from 'react-router-dom';
 import { AppRoutingPaths } from '@constants';
 
 import Home from "../../../assets/home.png";
-import HomeDiplomado from "../../../assets/IMG Login.png";
+import HomeDiplomado from "../../../assets/login_diplomado.png";
 import LogoLogin from "../../../assets/logo_ag_login.svg";
 import ContactoDialog from '../../molecules/Dialogs/ContactoDialog/ContactoDialog';
 import { useGetContacto } from '../../../services/ContactoService';
