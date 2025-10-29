5486711194ecc1c8f8655f9389e1215e45cc5148
ChampzHub <champz94tuxo@gmail.com>
2025-10-28 18:53:58 -0600

    add loaading Backdrop

--- src/components/pages/CursosActivosDetalles/Actividades.tsx
+++ src/components/pages/CursosActivosDetalles/Actividades.tsx
@@ -374,24 +374,7 @@ export const Actividades: React.FC = () => {
     return (
         <>
             {isSaving && (
-                <LoadingCircular
-                    Text="Guardando actividad..."
-                    sxProps={{
-                        position: 'fixed',
-                        top: 0,
-                        left: 0,
-                        right: 0,
-                        bottom: 0,
-                        zIndex: 2000,
-                        display: 'flex',
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                        height: '100vh',
-                        backgroundColor: 'rgba(0, 0, 0, 0.3)',
-                        backdropFilter: 'blur(6px)',
-                        WebkitBackdropFilter: 'blur(6px)',
-                    }}
-                />
+                <LoadingCircular Text="Guardando actividad..." isSaving />
             )}
             {
                 isMobile
