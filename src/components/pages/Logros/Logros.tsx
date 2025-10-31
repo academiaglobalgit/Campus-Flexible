72bbb16d829ec368dbdb02ff0206f7664fe7d1a6
ChampzHub <champz94tuxo@gmail.com>
2025-10-31 17:28:26 -0600

    add movil desing

--- src/components/pages/Logros/Logros.tsx
+++ src/components/pages/Logros/Logros.tsx
@@ -3,7 +3,7 @@ import { TitleScreen } from "@constants";
 import { TituloIcon } from "../../molecules/TituloIcon/TituloIcon";
 import { MisLogros as iconLogros } from "@iconsCustomizeds";
 import { Typography } from "../../atoms/Typography/Typography";
-import { Box, Card, CircularProgress, Divider, Tab, Tabs, tabsClasses, useMediaQuery } from "@mui/material";
+import { Box, Card, CircularProgress, Divider, LinearProgress, Tab, Tabs, tabsClasses, useMediaQuery } from "@mui/material";
 import { useGetDatosModulos } from "../../../services/ModulosCampusService";
 import { ModulosCampusIds } from "../../../types/modulosCampusIds";
 import { innerHTMLStyle } from "@styles";
@@ -29,10 +29,17 @@ import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper'
 import { formatFechaBonita } from "../../../utils/Helpers";
 import { DescripcionesPantallas } from '@constants';
+import { ContainerDesktop } from "../../organisms/ContainerDesktop/ContainerDesktop";
 
 interface MedallaProps {
     nivel: string;
     progreso: string | number;
+}
+
+type statsCard = {
+    title: string,
+    description: string,
+    color?: string,
 }
 
 const Logros: React.FC = () => {
@@ -69,25 +76,15 @@ const Logros: React.FC = () => {
         },
     }));
 
-    /* const LogrosCards: React.FC<{ horas: string, tipo: string, color: string }> = ({ horas, tipo, color }) => {
-        return (
-            <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', gap: '10px', width: isMobile ? '345px' : '188px', height: '92px', backgroundColor: color, borderRadius: '8px', marginBottom: '32px', marginTop: '36px', boxShadow: '0 4px 8px 0 rgba(107, 187, 228, 0.40)' }}>
-                <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', gap: '8px' }}>
-                    <Typography component="h4" variant="h4" sxProps={{ color: '#fff' }}> {horas} </Typography>
-                    <Typography component="h4" variant="h4" sxProps={{ color: '#fff' }}> Horas  </Typography>
-                </Box>
-                <Typography component="h4" variant="h4" sxProps={{ color: '#fff' }}> {tipo} </Typography>
-            </Box>
-        );
-    } */
-
-
     const Medalla: React.FC<MedallaProps> = ({ nivel, progreso }) => {
         const puntos = Number(progreso);
 
         let medallaSrc = medallaBronce;
         let mensaje = "";
 
+        if (puntos <= 3000) {
+            mensaje = "¡Sigue asi, estas a pocos pasos de llegar al siguiente nivel!";
+        }
         if (puntos > 3000) {
             medallaSrc = medallaOro;
             mensaje = "¡Increíble! Has alcanzado el nivel Avanzado";
@@ -103,22 +100,21 @@ const Logros: React.FC = () => {
                     flexDirection: "column",
                     justifyContent: "center",
                     alignItems: "center",
-                    gap: "10px",
-                    width: "386px",
+                    width: isMobile ? '90%' : "386px",
                     textAlign: "center",
                 }}
             >
-                <Box component="img" src={medallaSrc} alt="medalla"/>
+                <Box component="img" src={medallaSrc} alt="medalla" sx={{ marginBottom: '10px' }} />
 
                 <Typography component="h2" variant="h5" color="primary">
                     Nivel: {nivel}
                 </Typography>
 
-                <Typography component="span" variant="body1" color="text">
+                <Typography component="span" variant="body2" color="text">
                     {mensaje}
                 </Typography>
 
-                {/* <Box sx={{ width: "100%", mt: 2 }}>
+                <Box sx={{ width: "100%", mt: 2 }}>
                     <LinearProgress
                         variant="determinate"
                         value={Math.min(puntos / 4600 * 100, 100)}
@@ -132,14 +128,14 @@ const Logros: React.FC = () => {
                             },
                         }}
                     />
-                </Box> */}
+                </Box>
             </Box>
         );
     };
 
     const Insignia = (racha: string, fecha: string) => {
         return (
-            <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '10px', maxWidth: '276px', padding: '10px', }}>
+            <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', maxWidth: '276px', padding: '10px', }}>
                 <Box
                     component="img"
                     src={medallaBronce}
@@ -159,7 +155,7 @@ const Logros: React.FC = () => {
 
     const Certificaciones = (title: string, fecha: string, download: string) => (
         <>
-            <Box sx={{ display: 'flex', flexDirection: isMobile ? 'column' : 'row', justifyContent: 'space-between', mt: 1, gap: 2 }}>
+            <Box sx={{ display: 'flex', flexDirection: isMobile ? 'column' : 'row', justifyContent: 'space-between', gap: 2 }}>
 
                 <Box sx={{ display: 'flex', flexDirection: 'column' }}>
 
@@ -172,7 +168,7 @@ const Logros: React.FC = () => {
                     <Typography component="h5" variant="h5" color="primary"> Certificación en {title} </Typography>
                 </Box>
 
-                <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'flex-end', width: '350px' }}>
+                <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: isMobile ? 'flex-start' : 'flex-end', width: '350px' }}>
 
                     <Button disabled={download === null || download === '' ? true : false} onClick={() => handleDescargar(download)} sxProps={{ width: '140px' }}>
                         Ver detalles
@@ -196,10 +192,13 @@ const Logros: React.FC = () => {
         document.body.removeChild(link);
     };
 
+    const handleChange = (_event: React.SyntheticEvent, newValue: number) => {
+        setValue(newValue);
+    };
 
     const tabs = (recursos: any) => {
         return <>
-            <Box>
+            <Box sx={{ display: 'flex' }}>
                 <Tabs
                     value={value}
                     onChange={handleChange}
@@ -215,7 +214,7 @@ const Logros: React.FC = () => {
                 >
                     <Tab sx={{ minWidth: '150px', padding: '0px' }} label="Mis Certificaciones" />
                     <Tab sx={{ minWidth: '150px', padding: '0px' }} label="Mis Insignias" />
-                    {/*<Tab sx={{ minWidth: '150px', padding: '0px' }} label="Mi Clasificación" /> */}
+                    <Tab sx={{ minWidth: '150px', padding: '0px' }} label="Mi Clasificación" />
                 </Tabs>
             </Box>
 
@@ -245,8 +244,7 @@ const Logros: React.FC = () => {
                             flexDirection: 'column',
                             justifyContent: 'start',
                             width: '100%',
-                            m: 1,
-                            gap: '30px',
+                            gap: '10px',
                         }}
                     >
                         <Box sx={{ display: 'flex', mt: 1, flexDirection: 'row', alignItems: 'center', gap: 4 }}>
@@ -256,7 +254,7 @@ const Logros: React.FC = () => {
                             <ArrowForwardIosIcon />
                         </Box>
 
-                        <Box sx={{ display: 'flex', mt: 1, flexDirection: 'column', gap: 4 }}>
+                        <Box sx={{ display: 'flex', mt: 1, flexDirection: 'column', gap: 2 }}>
                             {calificacionData?.cursos?.flatMap(periodo =>
                                 periodo.cursos.map(curso =>
                                     Certificaciones(curso.nombre_curso, curso.fecha_registro, curso.url_accredible ?? '')
@@ -267,9 +265,10 @@ const Logros: React.FC = () => {
                 )}
             </TabPanel>
 
-
             <TabPanel value={value} index={1} dir={theme.direction}>
-                <Box sx={{ display: 'flex', flexDirection: 'column', width: '100%', backgroundColor: '#F1F4F6', borderRadius: '20px' }}>
+                <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', backgroundColor: '#F1F4F6', borderRadius: '20px' }}>
+
+                    {isMobile && <Medalla nivel="Principiante" progreso="100" />}
 
                     <Box sx={{ display: 'flex', mt: 1, flexDirection: isMobile ? 'column' : 'row', alignItems: 'center', justifyContent: isMobile ? 'center' : 'space-between', gap: 4, m: 2 }}>
 
@@ -306,11 +305,12 @@ const Logros: React.FC = () => {
             <TabPanel value={value} index={2} dir={theme.direction}>
                 <Box sx={{ display: 'flex', flexDirection: 'column', width: '100%', backgroundColor: '#F1F4F6', height: '350px', borderRadius: '20px' }}>
                     <Box sx={{ display: 'flex', mt: 1, flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', gap: 4, m: 2 }}>
+
                         <Box sx={{ display: 'flex', mt: 1, flexDirection: 'row', alignItems: 'center', gap: 4, m: 2 }}>
                             <Typography component="h5" variant="h5" sxProps={{ color: theme.palette.text.secondary }}> Clasificación General </Typography><ArrowForwardIosIcon />
                         </Box>
 
-                        <Box sx={{ display: 'flex', mt: 1, flexDirection: 'row', alignItems: 'center', gap: 4, m: 2, backgroundColor: 'black', padding: '10px 30px', borderRadius: '10px' }}>
+                        {!isMobile && <Box sx={{ display: 'flex', mt: 1, flexDirection: 'row', alignItems: 'center', gap: 4, m: 2, backgroundColor: 'black', padding: '10px 30px', borderRadius: '10px' }}>
                             <Typography component="span" variant="body3" sxProps={{ color: '#fff' }}>No. 123</Typography>
                             <Box
                                 component="img"
@@ -318,7 +318,7 @@ const Logros: React.FC = () => {
                                 alt="medalla"
                                 sx={{ width: '25px', height: '25px' }}
                             />
-                        </Box>
+                        </Box>}
 
                     </Box>
 
@@ -365,12 +365,6 @@ const Logros: React.FC = () => {
         { title: '5 cursos', description: 'Concluidos', color: 'warning.main' },
     ]
 
-    type statsCard = {
-        title: string,
-        description: string,
-        color?: string,
-    }
-
     const StatsCard = ({
         title,
         description,
@@ -382,9 +376,7 @@ const Logros: React.FC = () => {
                 flexDirection: 'column',
                 justifyContent: 'center',
                 gap: '16px',
-                maxWidth: '280px',
-                marginTop: '20px',
-                marginBottom: '20px',
+                maxWidth: isMobile ? '100%' : '188px',
                 width: '100%',
             }}
         >
@@ -392,20 +384,20 @@ const Logros: React.FC = () => {
                 sx={{
                     backgroundColor: `${color}`,
                     borderRadius: '12px',
-                    padding: '20px',
+                    padding: '12px 22px',
                     textAlign: 'center',
                     boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.1)'
                 }}
             >
                 <Typography
-                    variant="h3"
-                    component="div"
+                    variant="h5"
+                    component="span"
                     sxProps={{
-                        color: '#fff', 
-                        fontWeight: 'bold', 
+                        color: '#fff',
+                        fontWeight: 'bold',
                         marginBottom: '4px',
                     }}
-                    // sx={{ color: '#fff', fontWeight: 'bold', marginBottom: '4px' }}
+                // sx={{ color: '#fff', fontWeight: 'bold', marginBottom: '4px' }}
                 >
                     {title}
                 </Typography>
@@ -416,7 +408,7 @@ const Logros: React.FC = () => {
                         color: '#fff',
                         opacity: 0.9
                     }}
-                    // sx={{ color: '#fff', opacity: 0.9 }}
+                // sx={{ color: '#fff', opacity: 0.9 }}
                 >
                     {description}
                 </Typography>
@@ -427,26 +419,31 @@ const Logros: React.FC = () => {
 
     const Insignias = (
         <Box>
-            <TituloIcon Titulo={TitleScreen.LOGROS} Icon={isMobile && iconLogros} fontSize="h2" />
-            <Box sx={{ ...innerHTMLStyle, pl: 0, pr: 0 }} dangerouslySetInnerHTML={{ __html: Logros?.data?.descripcion_html ?? DescripcionesPantallas.LOGROS }} />
-            <Box
-                sx={{
-                        width: '100',
-                        pl: 0,
-                        pr: 0
-                    }}
-            >
-                {
-                    Stats.map((item, index) => (
-                        <StatsCard
-                            key={index}
-                            title={item.title}
-                            description={item.description}
-                            color={item.color}
-                        />
-                    ))
-                }
+
+            <Box sx={{ display: 'flex', flexDirection: 'row', gap: '60px' }}>
+
+                <Box sx={{ display: 'flex', flexDirection: 'column', gap: '30px' }}>
+                    <Box sx={{ ...innerHTMLStyle, pl: 0, pr: 0 }} dangerouslySetInnerHTML={{ __html: Logros?.data?.descripcion_html ?? DescripcionesPantallas.LOGROS }} />
+                    {!isMobile && <Typography component="h4" variant="h4" color="primary">
+                        Mi progreso en plataforma
+                    </Typography>}
+                    <Box sx={isMobile ? { display: 'flex', flexDirection: 'column', gap: '30px' } : { display: 'flex', gap: '30px' }}>
+                        {
+                            Stats.map((item, index) => (
+                                <StatsCard
+                                    key={index}
+                                    title={item.title}
+                                    description={item.description}
+                                    color={item.color}
+                                />
+                            ))
+                        }
+                    </Box>
+                </Box>
+
+                {!isMobile && <Medalla nivel="Principiante" progreso="100" />}
             </Box>
+
             <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'center' }}>
                 <Box sx={
                     [
@@ -460,24 +457,20 @@ const Logros: React.FC = () => {
         </Box>
     );
 
-    const handleChange = (_event: React.SyntheticEvent, newValue: number) => {
-        setValue(newValue);
-    };
-
     return (
 
         isLoading
             ?
-            <Box sx={{ 
-                        display: 'flex', 
-                        flexDirection: 'column', 
-                        gap: '20px', 
-                        width: '100%', 
-                        height: '50vh', 
-                        overflow: 'hidden', 
-                        justifyContent: 'center', 
-                        alignItems: 'center' 
-                    }}>
+            <Box sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                gap: '20px',
+                width: '100%',
+                height: '50vh',
+                overflow: 'hidden',
+                justifyContent: 'center',
+                alignItems: 'center'
+            }}>
                 <CircularProgress />
                 <Typography component="h4" variant="h4" color="primary">
                     Cargando logros...
@@ -486,20 +479,22 @@ const Logros: React.FC = () => {
             isMobile
                 ?
                 <>
-                    {Insignias}
-                    {tabs(recursos)}
+                    <Box sx={{ display: 'flex', flexDirection: 'column', gap: '15px' }}>
+                        <TituloIcon Titulo={TitleScreen.LOGROS} Icon={isMobile && iconLogros} fontSize="h2" />
+                        {Insignias}
+                        {tabs(recursos)}
+                    </Box>
                 </>
                 :
                 <>
-                    <Box sx={{ display: 'flex', flexDirection: 'column', height: 'auto' }} className='is1366'>
-                        <Box sx={{ display: 'flex' }}>
-                            {Insignias}
-                            <Box>
-                                <Medalla nivel="Principiante" progreso="0" />
+                    <ContainerDesktop title={TitleScreen.LOGROS} >
+                        <Box sx={{ display: 'flex', flexDirection: 'column', height: 'auto', gap: '15px' }}>
+                            <Box sx={{ display: 'flex' }}>
+                                {Insignias}
                             </Box>
+                            {tabs(recursos)}
                         </Box>
-                        {tabs(recursos)}
-                    </Box>
+                    </ContainerDesktop>
                 </>
     );
 };
